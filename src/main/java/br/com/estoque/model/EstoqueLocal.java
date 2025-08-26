e33cf963f0f2082c4f426ed5da7654db807e9be3
vitor <vtteixeira02@gmail.com>
2025-08-26 20:57:17 -0300

    Criação da classe EstoqueLocal

---
+++ src/main/java/br/com/estoque/model/EstoqueLocal.java
@@ -0,0 +1,61 @@
+package br.com.estoque.model;
+
+public class EstoqueLocal {
+
+    private Integer idEstoqueLocal;
+    private Integer idProduto;
+    private Integer idLocal;
+    private Integer quantidade;
+
+    public EstoqueLocal(){}
+
+    public EstoqueLocal(Integer idProduto, Integer idLocal, Integer quantidade){
+        this.idProduto = idProduto;
+        this.idLocal = idLocal;
+        this.quantidade = quantidade;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+    public void setQuantidade(Integer quantidade) {
+        if (quantidade != null && quantidade < 0) {
+            throw new IllegalArgumentException("Quantidade não pode ser negativa");
+        }
+        this.quantidade = quantidade;
+    }
+
+    public Integer getIdLocal() {
+        return idLocal;
+    }
+
+    public void setIdLocal(Integer idLocal) {
+        this.idLocal = idLocal;
+    }
+
+    public Integer getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(Integer idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public Integer getIdEstoqueLocal() {
+        return idEstoqueLocal;
+    }
+
+    public void setIdEstoqueLocal(Integer idEstoqueLocal) {
+        this.idEstoqueLocal = idEstoqueLocal;
+    }
+
+    @Override
+    public String toString() {
+        return "EstoqueLocal{" +
+                "idEstoqueLocal=" + idEstoqueLocal +
+                ", idProduto=" + idProduto +
+                ", idLocal" + idLocal +
+                ", Quantidade=" + quantidade +
+                '}';
+    }
+}
